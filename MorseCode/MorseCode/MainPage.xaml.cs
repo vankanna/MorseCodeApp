8ab5ccc4299892d1031acea30716ef47c07bced9
vankanna <passvankanna@gmail.com>
2022-04-05 21:38:27 -0500

    done

---
+++ MorseCode/MorseCode/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MorseCode
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private string chunk = "";
+        private void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            Button button = sender as Button;
+
+            char response;
+
+            if (button1 == button)
+            {
+                chunk += ".";
+                dotdash.Text += ".";
+            } 
+            else if (button2 == button)
+            {
+                chunk += "-";
+                dotdash.Text += "-";
+            }
+            else
+            {
+                if (chunk == "")
+                {
+                    dotdash.Text += " ";
+                }
+                response = Morse.MorseCoder(chunk);
+                userinput.Text += Char.ToString(response);
+                chunk = "";
+            }
+
+
+        }
+
+
+    }
+
+    
+
+}
